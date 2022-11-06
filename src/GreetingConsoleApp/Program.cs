5fbd600ace027ac5a1081084abef74165846761b
DevJonie <johnaddo.dev@gmail.com>
2022-11-06 13:30:42 +0000

    Add Geeter Console App

---
+++ src/GreetingConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using Akka.Actor;
+
+var actorSystem = ActorSystem.Create("the-universe");
+
+var greeter = actorSystem.ActorOf<GreetingActor>("greeter");
+
+greeter.Tell(new Greet("John"));
+
+Thread.Sleep(5000);
+
+actorSystem.Stop(greeter);
+
+Console.ReadLine();
+
+
+public class Greet 
+{
+    public Greet(string who)  => Who = who;
+    public string Who { get; }
+}
+
+public class GreetingActor : ReceiveActor 
+{
+    public GreetingActor() 
+    {
+        Receive<Greet>(greet => Console.WriteLine($"Hello {greet.Who}")); 
+    }
+
+    protected override void PreStart() =>
+        Console.WriteLine("Good morning, we are awake!", ConsoleColor.Green);
+
+    protected override void PostStop() =>
+    Console.WriteLine("Good night, going to bed!", ConsoleColor.Red);
+
+}
